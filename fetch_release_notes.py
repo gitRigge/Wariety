3d4cf88b65842ded4458a8d9646556885cc62237
Roland Rickborn (gitRigge) <r_2@gmx.net>
2020-06-04 19:05:22 +0200

    Print status while fetching release notes.

--- fetch_release_notes.py
+++ fetch_release_notes.py
@@ -3,11 +3,14 @@
 
 import sys
 from subprocess import check_output
+print('FETCH RELEASE NOTES: Read properties')
 release_notes = check_output('git log --all --grep="#releasenotes" --pretty=format:"%s (%h)"', shell=True).decode()
 release_notes = release_notes.replace(' #releasenotes','')
 
+print('FETCH RELEASE NOTES: Write release notes')
 f = open('Release Notes.txt', 'w')
 f.write(release_notes)
 f.close()
 
+print('FETCH RELEASE NOTES: Successfully finished')
 sys.exit()
