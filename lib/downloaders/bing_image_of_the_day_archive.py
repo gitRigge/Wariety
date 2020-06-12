813d4672099903cf74866e36817c4ae98eb85879
Roland Rickborn (gitRigge) <r_2@gmx.net>
2020-06-12 23:17:33 +0200

    Add additional built-in downloader 'Bing Image Of The Day Archive'

---
+++ lib/downloaders/bing_image_of_the_day_archive.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import datetime
+import json
+import logging
+import sys
+import urllib.parse
+
+import requests
+
+logger = logging.getLogger(__name__)
+if getattr(sys, 'frozen', False):
+    import wariety_wallpaper
+    from lib.downloaders.default_downloader import DefaultDownloader
+else:
+    import lib.wariety_wallpaper as wariety_wallpaper
+    from lib.downloaders.default_downloader import DefaultDownloader
+
+
+START_URL = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
+BASE_URL = 'https://www.bing.com'
+DOWNLOADER_TYPE = 'bingarchive'
+DOWNLOADER_DESCRIPTION = 'Bing Image Of The Day Archive'
+CAPABILITIES = {'single': 'single', 'many': 'many'}
+
+
+class BingDownloader(DefaultDownloader):
+
+    def __init__(self, config=None):
+        self.config = config
+        self._load_state(DOWNLOADER_TYPE)
+        super().__init__(config)
+
+    def __del__(self):
+        self.save_state(DOWNLOADER_TYPE)
+
+    def get_downloader_type(self):
+        return DOWNLOADER_TYPE
+
+    def get_downloader_description(self):
+        return DOWNLOADER_DESCRIPTION
+
+    def get_capability(self):
+        return CAPABILITIES['many']
+
+    def get_base_url(self):
+        return BASE_URL
+
+    def get_calculated_idx(self):
+        lastidx = self.state['idx']
+        laststartdate = self.state['startdate']
+        now_dtime = datetime.datetime.now()
+        laststartdate_dtime = datetime.datetime.strptime(self.state['startdate'], '%Y%m%d')
+        diff = now_dtime - laststartdate_dtime
+        return diff.days + lastidx + 1
+
+    def get_next_image(self, last_image_counter=0):
+        """
+        Retrieves a BING picture of the day and returns it
+        as an instance of wariety wallpaper. Depending on the counter
+        given by 'last_image_counter' returns the next picture.
+        :param last_image_counter:
+        :return next_image:
+        """
+        my_idx = self.get_calculated_idx()
+        my_start_url = START_URL.replace('0', my_idx)
+        next_image = wariety_wallpaper.WarietyWallpaper()
+        response = requests.get(my_start_url)
+        image_data = json.loads(response.text)
+
+        image_url = image_data["images"][0]["url"]
+        image_url = image_url.split("&")[0]
+        image_title = image_data["images"][0]["title"]
+        image_copyright = image_data["images"][0]["copyright"]
+        if image_title:
+            image_title = image_title + '.'
+        if image_copyright:
+            image_copyright = image_copyright + '.'
+        try:
+            next_image.image_name = urllib.parse.unquote(urllib.parse.urljoin(BASE_URL, image_url)).split('/')[-1].split('=')[-1]
+        except:
+            next_image.image_name = ''
+        next_image.source_url = urllib.parse.unquote(image_url)
+        next_image.source_type = DOWNLOADER_TYPE
+        next_image.image_author = ''
+        next_image.source_name = 'Bing Bild des Tages Archiv'
+        next_image.image_url = urllib.parse.unquote(urllib.parse.urljoin(BASE_URL, image_url))
+        next_image.location = ''
+        next_image.keywords = '{0} {1}'.format(image_title,image_copyright).strip()
+        next_image.source_location = ''
+        next_image.found_at_counter = last_image_counter + 1
+        self.state['last_image_counter'] = next_image.found_at_counter
+        startdate = datetime.datetime.now().strftime('%Y%m%y')
+        self.state['startdate'] = startdate
+        self.state['idx'] = my_idx
+        return next_image
