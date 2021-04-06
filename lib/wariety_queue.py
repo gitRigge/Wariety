9bb72d63c45e79a69d55e16877011c895ff7f255
Roland Rickborn (gitRigge) <r_2@gmx.net>
2021-04-06 19:01:28 +0200

    Add messageing to queue images

--- lib/wariety_queue.py
+++ lib/wariety_queue.py
@@ -17,48 +17,77 @@
 # along with this program.  If not, see https://www.gnu.org/licenses/.
 
 import logging
+import random
+
+from pubsub import pub
+
+import wariety_database
 
 logger = logging.getLogger(__name__)
 
-
-def to_queue(list_of_values, queue):
-    logger.debug('to_wallpaper()')
+def to_queue_item(list_of_values, queue_item):
+    logger.debug('to_queue_item()')
     i = 0
-    queue.id = list_of_values[i]
+    queue_item.image_id = list_of_values[i]
     i += 1
-    queue.image_id = list_of_values[i]
-    return queue
+    queue_item.queue_rank = list_of_values[i]
+    i += 1
+    queue_item.queue_status = list_of_values[i]
+    i += 1
+    queue_item.queue_seen_date = list_of_values[i]
+    i += 1
+    queue_item.previous_queue_id = list_of_values[i]
+    return queue_item
 
 class WarietyQueue(object):
     """docstring for WarietyQueue"""
 
     _instance = None
 
-    weight_unseen = 0
-    weight_rating = 0
-    weigth_total_seen = 0
-    weight_last_seen = 0
-    weight_random = 0
+    weight_unseen = 2
+    weight_rating = 3
+    weight_total_seen = 1
+    weight_last_seen = 1
+    weight_random = 3
 
     queue_statuses = {
         'QUEUED': 'QUEUED',
-        'DONE': 'DONE'
+        'DONE': 'DONE',
+        'CURRENT': 'CURRENT'
     }
 
-    def __init__(self):
+    def __init__(self, config={}):
         raise RuntimeError('Call instance() instead')
 
     @classmethod
-    def instance(cls):
+    def instance(cls, config={}):
         if cls._instance is None:
-            print('Creating new instance')
+            # Singleton
             cls._instance = cls.__new__(cls)
             logger.debug('Starting queue')
             logger.debug('__init__()')
+
+            # Configuration
+            cls.config = config
+
+            # Queue Item properties
             cls.id = ''
             cls.image_id = ''
-            cls.rank = 0
-            cls.status = 'QUEUED'
+            cls.queue_rank = 0
+            cls.queue_status = cls.queue_statuses['QUEUED']
+            cls.queue_seen_date = ''
+            cls.previous_queue_id = ''
+
+            # Queue Rankings
+            cls.queue_images = []
+            cls.queue_rankings = {}
+
+            # DB
+            cls.database = wariety_database.WarietyDatabase()
+
+            # Messaging
+            pub.subscribe(cls.update_queuer, "empty queue")
+
         return cls._instance
 
     def __del__(self):
@@ -67,10 +96,91 @@ class WarietyQueue(object):
 
     def to_dict(self):
         logger.debug('to_dict()')
-        my_queue = {
+        queue_item_dict = {
             'id': ['integer primary key', self.id],
             'image_id': ['integer', self.image_id],
-            'rank': ['integer', self.rank],
-            'status': ['text', self.status]
+            'queue_rank': ['integer', self.queue_rank],
+            'queue_status': ['text', self.queue_status],
+            'queue_seen_date': ['text', self.queue_seen_date],
+            'previous_queue_id': ['integer', self.previous_queue_id]
         }
-        return my_queue
+        return queue_item_dict
+
+    def update_queuer(self, msg):
+        """
+        Starts the process of receiving wallpaper images from the database, calculating their rankings and
+        sending them to the queue.
+        Is called by push message 'empty queue'
+        :param msg:
+        :return:
+        """
+        logging.debug('update_downloader(msg)')
+        self.get_queue_images()
+        self.calculate_queue_image_rankings()
+        self.send_queue_images_to_database()
+
+    def get_queue_images(self):
+        """
+        Receives a number of wallpapers from the database and fills it in 'queue_images'. The received
+        number of images depend on the weights.
+        :return:
+        """
+        logger.debug('get_queue_images()')
+
+        _seen = 0
+        self.queue_images = self.queue_images + self.database.get_seen_image(self.weight_unseen, _seen)
+        _rating = 5
+        self.queue_images = self.queue_images + self.database.get_rated_image(self.weight_rating, _rating)
+        _total_seen = 3
+        self.queue_images = self.queue_images + self.database.get_seen_image(self.weight_total_seen, _total_seen)
+        self.queue_images = self.queue_images + self.database.get_most_recent_seen_image(self.weight_last_seen)
+        self.queue_images = self.queue_images + self.database.get_random_image(self.weight_random)
+
+    def calculate_queue_image_rankings(self):
+        """
+        Calculates the ranking for each image in 'queue_images' based on the weights, the total no. of
+        wallpaper images in the databases and the wallpaper change interval.
+        :return:
+        """
+        logger.debug('calculate_queue_image_rankings()')
+
+        _tmp = {}
+        _total_number_of_Images = int(self.database.get_total_number_of_images())
+        _wp_change_interval = int(self.config['wallpaper_change_interval'])
+        factor = _wp_change_interval * 60 * 24 / _total_number_of_Images
+        for image in self.queue_images:
+            _point = 0
+            _days_since_last_seen = int(self.database.get_days_since_last_seen_by_id(image.id))
+            # calculate points based on number of days since the image has been shown the last time
+            if _days_since_last_seen == -1:
+                _point = _point + 10 * factor * self.weight_unseen
+            else:
+                _point = _point + _days_since_last_seen * factor * self.weight_last_seen
+            _total_seens = int(image.total_seen_number)
+            if _total_seens == 0:
+                _point = _point + 10 * factor * self.weight_unseen
+            else:
+                _point = _point - _total_seens * factor * self.weight_total_seen
+            _ranking = int(image.image_rating)
+            _point = _point + _ranking * factor * self.weight_rating
+            _random = random.randint(0, 10)
+            _point = _point + _random * factor * self.weight_random
+            _tmp[image.id] = _point
+        self.queue_rankings = _tmp
+
+    def send_queue_images_to_database(self):
+        """
+        Adds each queue ranking to the database.
+        :return:
+        """
+        logger.debug('send_queue_images_to_database()')
+
+        for img_id in self.queue_rankings:
+            _image_id = img_id
+            _queue_rank = self.queue_rankings[img_id]
+            _queue_status = self.queue_status
+            _queue_seen_date = ''
+            _previous_queue_id = -1
+            _queue_values = [_image_id, _queue_rank, _queue_status, _queue_seen_date, _previous_queue_id]
+            _queue_item = to_queue_item(_queue_values, WarietyQueue())
+            self.database.add_item_to_queue(_queue_item)
